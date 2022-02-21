8d3adc9c7c5389b2e85f4ff3c28b794786a3c00c
3monkey <csalv.gonz@gmail.com>
2022-02-21 20:16:17 +0100

    Fin Parte 3 vistas

--- polls/views.py
+++ polls/views.py
@@ -37,7 +37,8 @@ def detail(request, question_id):
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s "
-	return HttpResponse(response % question_id)
+	return render(request, 'polls/results.html', {'response':response})
 
 def vote(request, question_id):
-	return HttpResponse("You're looking at the results of question %s " % question_id)
+	response = "You're looking at the results of question %s " % question_id
+	return render(request, 'polls/vote.html', {'response':response})
