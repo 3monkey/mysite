9fea411b2daecab81151e5e31fc212d16d401039
3monkey <csalv.gonz@gmail.com>
2022-02-21 20:57:06 +0100

    Formularios

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+#from django.template import loader
+from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from .models import Choice, Question
 
 # Create your views here.
 
@@ -13,5 +17,16 @@ def results(request, question_id):
 	response = "You're looking at the results of question %s "
 	return HttpResponse(response % question_id)
 
-def vote(request, question_id):
-	return HttpResponse("You're looking at the results of question %s " % question_id)
+def vote(request, question_id):HttpResponse("You're looking at the results of question %s " % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (keyError, Choice.DoesNotExist):
+		return render(request, 'polls/details.html', {
+				'question':question,
+				'error_message' : "You didn't select a choice"
+			})
+	else:
+		selected_choice += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
